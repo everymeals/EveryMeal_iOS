4996cbe59214188c891a2b99138faabf81d66144
ios-kkr-private <kwangrok92@naver.com>
2023-07-23 17:22:56 +0900

    Add 프로젝트 생성

---
+++ EveryMeal_iOS/EveryMeal_iOS/EveryMeal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryMeal_iOSApp.swift
+//  EveryMeal_iOS
+//
+//  Created by 김광록 on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EveryMeal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
