ae634ce74192af24f2edfbc9598028fbd1e52492
ios-kkr-private <kwangrok92@naver.com>
2023-08-06 17:18:04 +0900

    Add Product Colors Extensions

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Utils/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  EveryMeal_iOS
+//
+//  Created by 김광록 on 2023/08/06.
+//
+
+import SwiftUI
+
+extension Color {
+  static let redLight = Color(red: 1, green: 0.93, blue: 0.93)
+  static let everyMealRed = Color(red: 1, green: 0.28, blue: 0.28)
+  static let redDark = Color(red: 0.8, green: 0.22, blue: 0.22)
+  static let greenLight = Color(red: 0.94, green: 0.98, blue: 0.96)
+  static let everyMealGreen = Color(red: 0.08, green: 0.77, blue: 0.49)
+}
