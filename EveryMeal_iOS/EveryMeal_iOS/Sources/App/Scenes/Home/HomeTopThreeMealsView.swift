31c512d1e76c5d941f67d8fc64b885b9f96b66c7
HaneulKim <khnpoq@gmail.com>
2023-08-08 01:35:47 +0900

    Add 홈탭 맛집 모아보기, 리뷰 모아보기 파일 추가

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/App/Scenes/Home/HomeTopThreeMealsView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTopThreeMealsView.swift
+//  EveryMeal_iOS
+//
+//  Created by 김하늘 on 2023/08/08.
+//
+
+import SwiftUI
+
+struct HomeTopThreeMealsView: View {
+  var body: some View {
+    VStack {
+      Text("맛집 모아보기")
+    }
+  }
+}
+
+struct HomeTopThreeMealsView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
