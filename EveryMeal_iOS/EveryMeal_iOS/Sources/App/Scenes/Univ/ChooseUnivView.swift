93e3ef4b7ff7ebcea7f1667d6e703af21d9004d8
ios-kkr-private <kwangrok92@naver.com>
2023-08-07 11:36:21 +0900

    Update 대학교 선택 화면 LazyGrid 영역, 셀 추가

--- EveryMeal_iOS/EveryMeal_iOS/Sources/App/Scenes/Univ/ChooseUnivView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/App/Scenes/Univ/ChooseUnivView.swift
@@ -11,18 +11,117 @@ struct ChooseUnivView: View {
   @State var isSelected: Bool = false
   
   var body: some View {
-    VStack {
-      Text("반가워요!\n대학을 선택해주세요")
-        .font(Font.system(size: 24, weight: .bold))
-        .foregroundColor(Color(red: 0.2, green: 0.24, blue: 0.29))
-        .padding(.top, 76)
-        .padding(.leading, 24)
-        .padding(.trailing, 158)
-      Spacer()
+    VStack(spacing: 20) {
+      VStack(alignment: .leading, spacing: 30.0) {
+        Text("반가워요!\n대학을 선택해주세요")
+          .font(Font.system(size: 24, weight: .bold))
+          .foregroundColor(Color(red: 0.2, green: 0.24, blue: 0.29))
+          .padding(.top, 76)
+          .padding(.leading, 24)
+          .padding(.trailing, 158)
+        
+        UnivGridView(isSelected: $isSelected)
+      }
+      
       ChooseButtonView(isSelected: $isSelected)
     }
   }
 }
+
+struct UnivGridView: View {
+  
+  @Binding var isSelected: Bool
+  @State var selectedIndex: Int?
+
+  let univs = [
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+    "명지대",
+    "서울여대",
+    "성신여대",
+  ]
+  
+  let univsImageName = [
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+    "myongji",
+    "swu",
+    "sungshin",
+  ]
+  
+  let columns = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible())
+  ]
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: columns, spacing: 10) {
+        ForEach(univs.indices, id: \.self) { index in
+          VStack(spacing: 10) {
+            Image(univsImageName[index])
+              .resizable()
+              .frame(width: 36, height: 36)
+            Text(univs[index])
+              .font(.system(size: 14))
+              .foregroundColor(Color(red: 0.31, green: 0.35, blue: 0.41))
+          }
+          .padding(10)
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+          .background(index == selectedIndex ? Color.grey3 : Color.grey1)
+          .cornerRadius(10)
+          .onTapGesture {
+            if selectedIndex == index {
+              selectedIndex = nil
+            } else {
+              selectedIndex = index
+            }
+            isSelected = selectedIndex != nil
+            print("👆 tapped university cell: \(index)th \(univs[index])")
+          }
+        }
+      }
+      .padding(.horizontal, 24)
+    }
+  }
+}
+
 
 struct ChooseButtonView: View {
   @Binding var isSelected: Bool
@@ -48,29 +147,30 @@ struct AddUnivView: View {
     HStack(alignment: .center) {
       HStack(alignment: .center, spacing: 14) {
         Image("icon-chat-bubble-dots-mono")
-        .frame(width: 24, height: 24)
+          .frame(width: 24, height: 24)
         
         VStack(alignment: .leading, spacing: 2) {
           Text("여기에 없어요")
             .font(.system(size: 15, weight: .semibold))
             .foregroundColor(Color(red: 0.31, green: 0.35, blue: 0.41))
-
+          
           Text("학교 신청하러 가기")
             .font(.system(size: 14, weight: .medium))
             .foregroundColor(Color(red: 0.69, green: 0.72, blue: 0.76))
         }
       }
+      .padding(.horizontal, 10)
       
       Spacer()
       
       Image("icon-arrow-right-small-mono")
         .frame(width: 20, height: 20)
     }
-    .padding(.horizontal, 24)
-    .padding(.vertical, 14)
-    .frame(width: 331, alignment: .leading)
+    .frame(maxWidth: .infinity)
+    .padding()
     .background(Color(red: 0.95, green: 0.96, blue: 0.96))
     .cornerRadius(100)
+    .padding(.horizontal, 22)
   }
 }
 
@@ -86,7 +186,7 @@ struct SelectUnivButton: View {
       .foregroundColor(Color.white)
       .cornerRadius(12)
       .padding(.horizontal, 20)
-      .padding(.bottom, 20)
+      .padding(.bottom, 10)
   }
 }
 
@@ -95,4 +195,3 @@ struct ChooseUnivView_Previews: PreviewProvider {
     ChooseUnivView()
   }
 }
-
