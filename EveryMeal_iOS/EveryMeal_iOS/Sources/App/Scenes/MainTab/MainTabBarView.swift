c7b7983c5d44e4d2edc19b142b402e262727741f
ios-kkr-private <kwangrok92@naver.com>
2023-08-20 18:30:22 +0900

    Update 탭바에 불필요한 네비게이션바, 검색뷰 삭제

--- EveryMeal_iOS/EveryMeal_iOS/Sources/App/Scenes/MainTab/MainTabBarView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/App/Scenes/MainTab/MainTabBarView.swift
@@ -12,48 +12,42 @@ struct MainTabBarView: View {
   @State private var favoritesCount = 10
   
   var body: some View {
-    NavigationView {
-      TabView(selection: $selectedTab) {
-        HomeView()
-          .tabItem {
-            Image("icon-store-mono")
-              .renderingMode(.template)
-            Text("맛집")
-          }
-          .tag(0)
-        MapView()
-          .tabItem {
-            Image("icon-folk-knife-mono")
-              .renderingMode(.template)
-            Text("학식")
-          }
-          .tag(1)
-        ChooseUnivView()
-          .tabItem {
-            Image("icon-chat-bubble-question-mono")
-              .renderingMode(.template)
-            Text("뭐먹지")
-          }
-          .tag(2)
-        Text("마이")
-          .tabItem {
-            Image("icon-user-mono")
-              .renderingMode(.template)
-            Text("마이")
-          }
-          .tag(3)
-          .badge(favoritesCount)
-      }
-      .onAppear(perform: {
-        UITabBar.appearance().unselectedItemTintColor = UIColor(red: 0.69, green: 0.72, blue: 0.76, alpha: 1)
-      })
-      .navigationBarTitle(getNavigationTitle(), displayMode: .inline)
-      .navigationBarItems(
-        trailing: selectedTab == 0 ? NavigationLink(destination: SearchView()) {
-          Image(systemName: "magnifyingglass")
-        } : nil
-      )
+    TabView(selection: $selectedTab) {
+      HomeView()
+        .tabItem {
+          Image("icon-store-mono")
+            .renderingMode(.template)
+          Text("맛집")
+        }
+        .tag(0)
+      MapView()
+        .tabItem {
+          Image("icon-folk-knife-mono")
+            .renderingMode(.template)
+          Text("학식")
+        }
+        .tag(1)
+      Text("뭐먹지")
+        .tabItem {
+          Image("icon-chat-bubble-question-mono")
+            .renderingMode(.template)
+          Text("뭐먹지")
+        }
+        .tag(2)
+      Text("마이")
+        .tabItem {
+          Image("icon-user-mono")
+            .renderingMode(.template)
+          Text("마이")
+        }
+        .tag(3)
+        .badge(favoritesCount)
     }
+    .tabViewStyle(.automatic)
+    .onAppear(perform: {
+      UITabBar.appearance().unselectedItemTintColor = UIColor(red: 0.69, green: 0.72, blue: 0.76, alpha: 1)
+      UITabBar.appearance().isTranslucent = false
+    })
   }
   
   func getNavigationTitle() -> Text {
@@ -75,43 +69,6 @@ struct MainTabBarView: View {
     }
   }
 
-}
-
-struct SearchView: View {
-  @State private var searchText: String = ""
-  @State private var searchResults: [String] = []
-  
-  var body: some View {
-    VStack {
-      SearchBar(text: $searchText, onSearchButtonClicked: performSearch)
-        .padding(.horizontal)
-      
-      List(searchResults, id: \.self) { result in
-        Text(result)
-      }
-    }
-  }
-  
-  func performSearch() {
-    searchResults = ["Apple", "Apple Pie", "Apple Juice"].filter { $0.lowercased().contains(searchText.lowercased()) }
-  }
-}
-
-struct SearchBar: View {
-  @Binding var text: String
-  var onSearchButtonClicked: () -> Void
-  
-  var body: some View {
-    HStack {
-      TextField("검색어 입력", text: $text, onCommit: onSearchButtonClicked)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-      
-      Button(action: onSearchButtonClicked) {
-        Text("검색")
-      }
-      .padding(.horizontal)
-    }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
