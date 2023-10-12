806e62b367d7c955c430f6cfa4c47c6a3733679a
HaneulKim <khnpoq28@naver.com>
2023-10-12 17:47:12 +0900

    Add Constants, Funtions

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Utils/Functions.swift
@@ -0,0 +1,17 @@
+//
+//  Functions.swift
+//  EveryMeal_iOS
+//
+//  Created by 김하늘 on 10/12/23.
+//
+
+import Photos
+import UIKit
+
+class Functions {
+  // 앱 설정 페이지
+  public static func openAppSettings() {
+    guard let settingURL = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingURL) else { return }
+    UIApplication.shared.open(settingURL, options: [:])
+  }
+}
