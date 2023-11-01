e391bb0499768a21e8d5804d0dd92b2f5cbdaab7
HaneulKim <khnpoq@naver.com>
2023-11-01 14:25:09 +0900

    Update 추천, 카페, 밥집, 맛집 리스트 추가

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/MoreStoresView/MoreStoreView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/MoreStoresView/MoreStoreView.swift
@@ -7,28 +7,62 @@
 
 import SwiftUI
 
+enum MoreViewType: String {
+  case recommend = "추천"
+  case meal = "밥집"
+  case alcohol = "술집"
+  case coffee = "카페"
+  case best = "맛집"
+}
+
 struct MoreStoreView: View {
   var backButtonTapped: () -> Void
-  var mealModels: MealEntity
+  var moreViewType: MoreViewType
   
   var body: some View {
     NavigationView {
       VStack {
-        
         CustomNavigationView(
-          title: "추천",
+          title: moreViewType.rawValue,
           leftItem: Image("icon-arrow-left-small-mono"),
           leftItemTapped: {
             backButtonTapped()
           }
         )
-        MealHorizontalItemView(mealModel: mealModels)
         
-        MealGridView(didMealTapped: { _ in })
-          .padding(.top, 20)
-        
-        Spacer()
+        ScrollView {
+          VStack(spacing: 0) {
+            if moreViewType != .best {
+              VStack(spacing: 0) {
+                HStack {
+                  Text(moreViewType == .recommend ? "추천 맛집" : "추천 " + moreViewType.rawValue)
+                    .font(.system(size: 18, weight: .bold))
+                    .foregroundColor(.black)
+                  Spacer()
+                }
+                .padding(.leading, 20)
+                .padding(.top, 16)
+                .padding(.bottom, 15)
+                
+                MealHGridView(didMealTapped: { mealModel in
+                  print("가게 상세로 이동")
+                })
+                .padding(.horizontal, 20)
+                
+                Separator()
+              }
+            }
+            
+            FilterBarView(viewType: .stores)
+            
+            MealGridView(didMealTapped: { _ in })
+              .padding(.top, 20)
+            
+            Spacer()
+          }
+        }
       }
+      
     }
     .navigationBarHidden(true)
   }
@@ -38,7 +72,7 @@ struct MoreStoreView_Previews: PreviewProvider {
   static var previews: some View {
     MoreStoreView(backButtonTapped: {
       print("back")
-    }, mealModels: .init(title: "투텍스쳐 투텍스쳐투텍스쳐투텍스쳐투텍스쳐...", type: .양식, description: "fdsfds", score: 4.5, doUserLike: true, likesCount: 30))
+    }, moreViewType: .best)
   }
 }
 
