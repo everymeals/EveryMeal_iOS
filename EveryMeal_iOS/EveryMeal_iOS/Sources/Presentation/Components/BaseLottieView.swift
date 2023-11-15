57fdf67f2e405f2805237d81140107407ea24787
kkr <kwangrok92@naver.com>
2023-11-15 11:12:42 +0900

    Update PR 반영: UIKit 제거

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/BaseLottieView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/BaseLottieView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import UIKit
 import Lottie
 
 struct BaseLottieView: UIViewRepresentable {
@@ -14,27 +13,27 @@ struct BaseLottieView: UIViewRepresentable {
   var loopMode: LottieLoopMode = .loop
   
   func makeUIView(context: UIViewRepresentableContext<BaseLottieView>) -> UIView {
-      let view = UIView(frame: .zero)
-      
-      let animationView = LottieAnimationView()
-      let animation = LottieAnimation.named(jsonName)
-      animationView.animation = animation
-      animationView.contentMode = .scaleAspectFit
-      animationView.loopMode = loopMode
-      animationView.play()
-      
-      animationView.translatesAutoresizingMaskIntoConstraints = false
-      view.addSubview(animationView)
-      
-      NSLayoutConstraint.activate([
-          animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
-          animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
-      ])
-      
-      return view
+    let view = UIView(frame: .zero)
+    
+    let animationView = LottieAnimationView()
+    let animation = LottieAnimation.named(jsonName)
+    animationView.animation = animation
+    animationView.contentMode = .scaleAspectFit
+    animationView.loopMode = loopMode
+    animationView.play()
+    
+    animationView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(animationView)
+    
+    NSLayoutConstraint.activate([
+      animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+      animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+    ])
+    
+    return view
   }
   
   func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BaseLottieView>) {
-      // Use this function to update the view in response to SwiftUI state changes.
+    // Use this function to update the view in response to SwiftUI state changes.
   }
 }
