4b85e974fb8e160bfd7eedae71ffce5d491382c2
kkr <kwangrok92@naver.com>
2023-12-26 08:15:10 +0900

    Add 바텀시트 DocC

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/BottomSheet/CustomSheetView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/BottomSheet/CustomSheetView.swift
@@ -7,46 +7,30 @@
 
 import SwiftUI
 
-/// 사용 예시
-///```swift
-///.onTapGesture {
-///  isPresented.toggle()
-///}
-///.sheet(isPresented: $isPresented, content: {
-///  VStack {
-///    CustomSheetView(title: "무엇으로 신고하시나요?", buttonTitle: "확인", isPresented: $isSortOpened) {
-///      VStack {
-///        HStack {
-///          Text("해당 가게와 무관한 리뷰")
-///            .padding(.vertical, 14)
-///          Spacer()
-///          Image("icon-check-mono")
-///            .renderingMode(.template)
-///            .foregroundStyle(Color.grey4)
-///        }
-///        HStack {
-///          Text("비속어 및 혐오 발언")
-///            .padding(.vertical, 14)
-///          Spacer()
-///          Image("icon-check-mono")
-///            .renderingMode(.template)
-///            .foregroundStyle(Color.grey4)
-///        }
-///        HStack {
-///          Text("음란성 게시물")
-///            .padding(.vertical, 14)
-///          Spacer()
-///          Image("icon-check-mono")
-///            .renderingMode(.template)
-///            .foregroundStyle(Color.grey4)
-///        }
-///      }
-///    }
-///  }
-///  .presentationDetents([.height(330)])
-///  .presentationDragIndicator(.hidden)
-///})
-///```
+/// `CustomSheetView`는 선택적인 제목과 버튼 구성 요소를 제공하며, 커스텀 가능한 컨텐츠를 포함합니다.
+///
+/// - 매개변수:
+///   - title: 시트의 제목으로 사용될 선택적 `String`. 기본값은 `nil`.
+///   - buttonTitle: 하단 버튼의 제목으로 사용될 선택적 `String`. 기본값은 `nil`.
+///   - content: 시트에 표시될 컨텐츠를 반환하는 클로저.
+///   - buttonAction: 버튼의 동작을 정의하는 선택적 클로저. 기본값은 빈 클로저.
+///
+/// 이 구조체는 상단에 그래버 인디케이터, 선택적인 제목과 버튼, 그리고 커스텀 가능한 컨텐츠 영역으로 일관된 레이아웃을 제공합니다.
+/// 제공된 경우, 하단 버튼에는 시트를 닫는 기본 동작이 포함됩니다.
+/// 기기에 물리 홈 버튼이 있는 경우 버튼 하단에 추가적인 패딩이 적용됩니다.
+///
+/// 사용 예시:
+/// ```swift
+/// CustomSheetView(title: "샘플 제목", buttonTitle: "닫기") {
+///     Text("여기에 컨텐츠")
+/// } buttonAction: {
+///     // 여기에 버튼 동작 정의
+/// }
+/// ```
+///
+/// 뷰의 본문은 그래버, 제목, 컨텐츠, 버튼의 레이아웃을 관리하는 `VStack`을 포함합니다.
+/// 일반적인 시트 동작을 처리하기 위해 `.dismiss`와 같은 환경 값을 사용합니다.
+/// 초기화 매개변수를 통해 뷰의 외관과 동작을 커스텀할 수 있습니다.
 struct CustomSheetView<Content>: View where Content: View {
   @Environment(\.dismiss) var dismiss
 
