ce7f8f60768a3f01aa1a9b30a92feb5a6c7c4f52
HaneulKim <khnpoq@naver.com>
2023-12-27 18:30:15 +0900

    Add 카메라뷰 및 권한 추가

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/Review/Components/CameraPicker.swift
@@ -0,0 +1,55 @@
+//
+//  CameraPicker.swift
+//  EveryMeal_iOS
+//
+//  Created by 김하늘 on 12/27/23.
+//
+
+import UIKit
+import SwiftUI
+
+struct CameraPicker: UIViewControllerRepresentable {
+  typealias UIViewControllerType = UIImagePickerController
+  typealias Coordinator = CameraPickerCoordinator
+  
+  @Binding var image: Image?
+  @Binding var isActive: Bool
+  
+  func makeUIViewController(context: Context) -> UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.sourceType = .camera
+    picker.delegate = context.coordinator
+    picker.allowsEditing = true
+    
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) { }
+  
+  func makeCoordinator() -> CameraPickerCoordinator {
+    return CameraPickerCoordinator(image: $image, isActive: $isActive)
+  }
+}
+
+class CameraPickerCoordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  @Binding var image: Image?
+  @Binding var isActive: Bool
+  
+  init(image: Binding<Image?>, isActive: Binding<Bool>) {
+    _image = image
+    _isActive = isActive
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+      let image = Image(uiImage: uiImage)
+      self.image = image
+      self.isActive = false
+    }
+  }
+  
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    self.isActive = false
+  }
+}
--- EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/Review/Components/ImagePicker.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/Review/Components/ImagePicker.swift
@@ -27,33 +27,32 @@ struct ImagePicker: UIViewControllerRepresentable {
   func makeCoordinator() -> Coordinator {
     Coordinator(self)
   }
+}
+
+class Coordinator: PHPickerViewControllerDelegate {
+  private let parent: ImagePicker
   
-  class Coordinator: PHPickerViewControllerDelegate {
-    private let parent: ImagePicker
+  init(_ parent: ImagePicker) {
+    self.parent = parent
+  }
+  
+  func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+    print(results)
+    var fetchedImages: [Image] = []
     
-    init(_ parent: ImagePicker) {
-      self.parent = parent
-    }
-    
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-      print(results)
-      var fetchedImages: [Image] = []
-      
-      for result in results.enumerated() {
-        let provider = result.element.itemProvider
-        if provider.canLoadObject(ofClass: UIImage.self) {
-          provider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-            if let image = image as? UIImage {
-              fetchedImages.append(Image(uiImage: image))
-              if result.offset == results.count - 1 {
-                self?.parent.selectedImages = fetchedImages
-                self?.parent.isPresented = false
-              }
+    for result in results.enumerated() {
+      let provider = result.element.itemProvider
+      if provider.canLoadObject(ofClass: UIImage.self) {
+        provider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+          if let image = image as? UIImage {
+            fetchedImages.append(Image(uiImage: image))
+            if result.offset == results.count - 1 {
+              self?.parent.selectedImages = fetchedImages
+              self?.parent.isPresented = false
             }
           }
         }
       }
-      
     }
   }
 }
