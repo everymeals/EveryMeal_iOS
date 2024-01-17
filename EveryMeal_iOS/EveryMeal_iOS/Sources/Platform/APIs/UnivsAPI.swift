06e2d397fc1bc73361a18133cd098344a495c5ae
kkr <kwangrok92@naver.com>
2024-01-17 20:38:33 +0900

    Add 대학리스트 API

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Platform/APIs/UnivsAPI.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Platform/APIs/UnivsAPI.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Moya
+
+enum UnivsAPI {
+  case getUniversities
+}
+
+extension UnivsAPI: TargetType {
+  var baseURL: URL {
+    return URL(string: "http://dev.everymeal.shop:8085")!
+  }
+  
+  var path: String {
+    switch self {
+    case .getUniversities: 
+      return "/api/v1/universities"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .getUniversities: 
+      return .get
+    }
+  }
+  
+  var task: Moya.Task {
+    return .requestPlain
+  }
+  
+  var headers: [String : String]? {
+    return ["Content-type": "application/json"]
+  }
+  
+}
