319303b8706431acbdc30f26d23e6884caa2d85b
kkr <kwangrok92@naver.com>
2024-01-17 20:37:54 +0900

    Add UserDefaultsManager 임시 추가 (삭제될 수도 있음)

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Data/Storages/UserDefaultsManager.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Data/Storages/UserDefaultsManager.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-/// temporary file
-class UserDefaults {
-  
+/// FIXME: temporary file
+class UserDefaultsManager {
+  /// @AppStorage("isFirstLaunching") var isFirstLaunching: Bool = true
+  /// 위와 같이 쓰면 되는 것 같아서 필요없게 되면 삭제 처리할 것
+
 }
