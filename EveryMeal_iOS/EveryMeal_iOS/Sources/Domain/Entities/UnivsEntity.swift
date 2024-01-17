b047c3c83889581aa2d41b8d2122f81f2536c88d
kkr <kwangrok92@naver.com>
2024-01-17 20:38:58 +0900

    Add 대학리스트 Entity

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Domain/Entities/UnivsEntity.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Domain/Entities/UnivsEntity.swift
@@ -5,9 +5,11 @@
 //  Created by 김광록 on 1/17/24.
 //
 
-struct UnivsEntity: Decodable {
-    let idx: Int
-    let universityName: String
-    let campusName: String
-    let universityShortName: String
+struct UnivsEntity: Decodable, Identifiable {
+  let idx: Int
+  let universityName: String
+  let campusName: String
+  let universityShortName: String
+  
+  var id: Int { idx }
 }
