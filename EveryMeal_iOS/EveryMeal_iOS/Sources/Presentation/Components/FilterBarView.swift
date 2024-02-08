4679696a32164060b9ce67dcd56aca95279c2cdd
kkr <kwangrok92@naver.com>
2024-02-08 09:18:13 +0900

    Update 최신순, 필터 바 View에 completionHandler 추가

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/FilterBarView.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Components/FilterBarView.swift
@@ -16,6 +16,7 @@ struct FilterBarView: View {
   @State var viewType: ViewType = .reviews
   @State var isSortOpened = false
   @State var isFilterOpened = false
+  var completionHandler: (() -> Void)?
   
   var body: some View {
     HStack {
@@ -35,7 +36,8 @@ struct FilterBarView: View {
       .background(Color(red: 0.95, green: 0.96, blue: 0.96))
       .cornerRadius(100)
       .onTapGesture {
-        isSortOpened.toggle()
+//        isSortOpened.toggle()
+        completionHandler?()
       }
       .sheet(isPresented: $isSortOpened, content: {
         VStack {
@@ -113,7 +115,8 @@ struct FilterBarView: View {
       .background(Color(red: 0.95, green: 0.96, blue: 0.96))
       .cornerRadius(100)
       .onTapGesture {
-        isFilterOpened.toggle()
+        //isFilterOpened.toggle()
+        completionHandler?()
       }
       .alert(title: "이거는 버튼 하나 짜리\nddd\nddd\ndd", message: "하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하\n하하하하하하하", dismissButton: CustomAlertButton(title: "버튼\n두줄"), isPresented: $isFilterOpened)
       
