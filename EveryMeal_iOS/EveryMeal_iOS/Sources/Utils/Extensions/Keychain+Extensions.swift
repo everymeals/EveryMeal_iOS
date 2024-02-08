d452fdc336dd8b0c6a12997acd93504629bc4bdd
HaneulKim <khnpoq@naver.com>
2024-02-08 18:29:58 +0900

    Update keychain 키값 추가

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Utils/Extensions/Keychain+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  Keychain+Extensions.swift
+//  EveryMeal_iOS
+//
+//  Created by 김하늘 on 2/8/24.
+//
+
+import Foundation
+
+import KeychainSwift
+
+enum KeychainKey: String {
+  case accessToken
+  case refreshToken
+}
+
+extension KeychainSwift {
+  @discardableResult
+  func set(_ value: String, forKey key: KeychainKey,
+           withAccess access: KeychainSwiftAccessOptions? = nil) -> Bool {
+    
+    if let value = value.data(using: String.Encoding.utf8) {
+      return set(value, forKey: key.rawValue, withAccess: access)
+    }
+    
+    return false
+  }
+  
+  func get(_ key: KeychainKey) -> String? {
+    if let data = getData(key.rawValue) {
+      
+      if let currentString = String(data: data, encoding: .utf8) {
+        return currentString
+      }
+      
+      lastResultCode = -67853 // errSecInvalidEncoding
+    }
+
+    return nil
+  }
+}
