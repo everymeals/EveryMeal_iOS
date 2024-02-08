cb33c0ef6f1d60f1b598da96ebe328c63b5daabc
HaneulKim <khnpoq@naver.com>
2024-02-08 19:03:57 +0900

    Update store api bearer token 추가

--- EveryMeal_iOS/EveryMeal_iOS/Sources/Data/NetworkConfig/AuthInterceptor.swift
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Data/NetworkConfig/AuthInterceptor.swift
@@ -18,22 +18,6 @@ final class AuthInterceptor: RequestInterceptor {
   
   private init() {}
   
-//  func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-//    guard urlRequest.url?.absoluteString.hasPrefix(Config.baseURL) == true,
-//          let accessToken = UserManager.shared.accessToken,
-//          let refreshToken = UserManager.shared.refreshToken
-//    else {
-//      completion(.success(urlRequest))
-//      return
-//    }
-//    
-//    var urlRequest = urlRequest
-//    urlRequest.setValue(accessToken, forHTTPHeaderField: "accessToken")
-//    urlRequest.setValue(refreshToken, forHTTPHeaderField: "refreshToken")
-//    print("adator 적용 \(urlRequest.headers)")
-//    completion(.success(urlRequest))
-//  }
-  
   func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
     
     guard let response = request.task?.response as? HTTPURLResponse,
