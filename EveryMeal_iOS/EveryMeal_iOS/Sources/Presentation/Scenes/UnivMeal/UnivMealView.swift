f6df4320e37da02587855d260eea19a3cd1dfbc7
kkr <kwangrok92@naver.com>
2024-02-12 20:07:50 +0900

    Add UnivMealView

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/UnivMeal/UnivMealView.swift
@@ -0,0 +1,18 @@
+//
+//  UnivMealView.swift
+//  EveryMeal_iOS
+//
+//  Created by 김광록 on 2/12/24.
+//
+
+import SwiftUI
+
+struct UnivMealView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#Preview {
+  UnivMealView()
+}
