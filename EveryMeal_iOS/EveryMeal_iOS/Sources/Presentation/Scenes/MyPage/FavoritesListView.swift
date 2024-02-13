df84a1886a57b5f88b4f28ad5026cdb333c18226
kkr <kwangrok92@naver.com>
2024-02-14 08:50:47 +0900

    Add 설정 탭 저장페이지 공사중 이미지

---
+++ EveryMeal_iOS/EveryMeal_iOS/Sources/Presentation/Scenes/MyPage/FavoritesListView.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListView.swift
+//  EveryMeal_iOS
+//
+//  Created by 김광록 on 2/13/24.
+//
+
+import SwiftUI
+
+struct FavoritesListView: View {
+  var body: some View {
+    UnderConstruction()
+      .toolbar(.hidden, for: .tabBar)
+  }
+}
+
+#Preview {
+  FavoritesListView()
+}
